b5d485f2372f2fe0153442593a77f3d8797c8310
Ricardo Serrato B <rsbse@hotmail.com>
2022-11-21 14:51:55 -0600

    Changes

--- spinner.js
+++ spinner.js
@@ -1,13 +1,104 @@
+class Figure {
+    getPoints() { 
+        return [];
+    }
+}
+
+class OneLine extends Figure {
+    getPoints() {
+        let points = [];
+        let yCenter = window.innerHeight / 2;
+        let xCenter = window.innerWidth / 2;
+        let i = 0;
+        let j = 0; 
+    
+        while(i < 200) {       
+            points.push({x: xCenter + j, y: yCenter});
+    
+            i++;
+            j += 0.2;
+        }
+    
+        return points;
+    }
+}
+
+class TwoLines extends Figure {
+    getPoints() {
+        let yCenter = window.innerHeight / 2;
+        let xCenter = window.innerWidth / 2;
+        let points = [];
+        let i = 0;
+        let j = 0;
+    
+        while(i < 200) {
+    
+            points.push({x: xCenter + j, y: yCenter});
+            points.push({x: xCenter - j , y: yCenter});
+    
+            i++;
+            j += 0.2;
+        }
+    
+        return points;
+    }
+}
+
+class FourLines extends Figure {
+    getPoints() {
+        let yCenter = window.innerHeight / 2;
+        let xCenter = window.innerWidth / 2;
+        let points = [];
+        let i = 0;
+        let j = 0;
+    
+        while(i < 200) {
+       
+            points.push({x: xCenter + j, y: yCenter});
+            points.push({x: xCenter - j , y: yCenter});
+    
+            points.push({x: xCenter, y: yCenter + j});
+            points.push({x: xCenter , y: yCenter - j});
+    
+            i++;
+            j += 0.5;
+        }
+    
+        return points;
+    }
+}
+
+
+
+
+var figurePoints = [];
 var points = [];
-var x = window.innerWidth / 2;
-var increment = 0.5;
+var iteration = 0;
 var rotationAngle = 0;
-var incrementRotationAngle = 0.01;
+var incrementRotationAngle = 0.2;
+var colors = [
+    [204, 102, 255],
+    [255, 102, 102],
+    [255, 153, 51],
+    [255, 255, 102],
+    [153, 255, 51],
+    [0, 255, 0],
+    [0, 255, 255],
+    [51, 153, 255],
+    [153, 51, 255]
+];
+
+var figures = [
+    new OneLine(),
+    new TwoLines(),
+    new FourLines()
+]
 
 function setup() {
+    figurePoints = figures.pop().getPoints();
 	createCanvas(window.innerWidth, window.innerHeight);
 	stroke(255);     
-    strokeWeight(3)
+    strokeWeight(1)
 	background(0);
 }
 
@@ -15,30 +106,56 @@ function draw() {
     background(0);
     iterate();
     drawPoints();
+}
 
+function changeColor(colorIndex) {
+    if (colorIndex == colors.length) {
+        colorIndex = 0;
+    }
+
+    let color = colors[colorIndex];
+    stroke(color[0], color[1], color[2]); //RGB
+
+    colorIndex += 1;
+
+    return colorIndex;
 }
 
 function drawPoints() {
-    points.forEach(p => {
-        circle(p.x, p.y, 2);
+
+    let iColor = 0;
+    points.forEach(current => {
+
+        iColor = changeColor(iColor);
+        circle(current.x, current.y, 1);
     });
 }
 
 function iterate() {
-    let y = fnLine(x)
-    let point = {x: x, y: y};
-    point = rotatePoint(point, rotationAngle);
-    points.push(point);
-    x = x + increment;
+    if (!figures.length && !figurePoints.length) {
+        return;
+    }
+
+    if (iteration == figurePoints.length) {
+        figurePoints = figures.pop().getPoints();
+        iteration = 0;
+        points = [];
+    }
+
+    let figurePoint = figurePoints[iteration];
+    let rotatedPoint = rotatePoint(figurePoint, rotationAngle);
+    points.push(rotatedPoint);
+
+    iteration = iteration + 1;
     rotationAngle = rotationAngle + incrementRotationAngle;
 }
 
 function rotatePoint(point, angle) {
-    point.x = point.x - window.innerWidth / 2; 
-    point.y = point.y - window.innerHeight / 2;
+    let tx  = point.x - window.innerWidth / 2; 
+    let ty = point.y - window.innerHeight / 2;
 
-    let x = point.x * Math.cos(angle) - point.y * Math.sin(angle);
-    let y = point.y * Math.cos(angle) + point.x * Math.sin(angle);
+    x = tx * Math.cos(angle) - ty * Math.sin(angle);
+    y = ty * Math.cos(angle) + tx * Math.sin(angle);
 
     x = x + window.innerWidth / 2; 
     y = y + window.innerHeight / 2;
@@ -49,9 +166,4 @@ function rotatePoint(point, angle) {
     }
 }
 
-function fnLine(x) {
-    let m = 0;
-    let b = window.innerHeight / 2;
 
-    return m * x + b
-}
